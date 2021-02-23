5ba9d7fddd7bba8bb97ed56ec04298fe5f9b6823
Brian Wlazlo <brianwlazlo7@gmail.com>
2021-02-22 21:00:40 -0500

    added menu close on link click

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function transformMenu(x) {
   x.classList.toggle("change");
   $('.nav-links').toggleClass('hide');
 }
+
+function closeMenu(x) {
+  x.classList.toggle('hide');
+  $('.menu-icon').toggleClass('change');
+}
